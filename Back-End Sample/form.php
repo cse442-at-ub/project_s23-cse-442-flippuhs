c7c4e6b02f573137959c6f13c3dca4fc834411d9
chrjho <cho29@buffalo.edu>
2023-03-12 19:54:29 -0400

    added image uploading functionality to sample form #35

--- Back-End Sample/form.php
+++ Back-End Sample/form.php
@@ -22,7 +22,24 @@
                 <input type="text" name="email" id="emailAddress">
             </p>
 
-            <input type="submit" value="Submit">
+            <input type="submit" name="userData" value="Submit">
+        </form>
+        <br>
+        <br>
+        <h1>Uploading Image Form</h1>
+        <form action="database.php" method="post" enctype="multipart/form-data">
+            <input type="text" placeholder="Username" name="username" id="userName1">
+            <br>
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" name="uploadImage" value="Upload Image">
+        </form>
+        <br>
+        <br>
+        <h1>Display Profile Picture</h1>
+        <form action="database.php" method="post" enctype="multipart/form-data">
+            <input type="text" placeholder="Username" name="username" id="userName2">
+            <input type="submit" name="getProfilePic" value="Display PFP">
         </form>
     </center>
 </body>
