44def7b0e9b2e2641175fa691c66656adf237f36
chrjho <cho29@buffalo.edu>
2023-03-17 20:41:14 -0400

    fix redirect and cookie bug #42

--- Back-End/Login.php
+++ Back-End/Login.php
@@ -10,9 +10,12 @@ if (isset($_POST['login'])) {
     $pass = $_POST['password'];
 
     if (password_verify($pass, $dbConn->getUserPassword($user))){
+        if (isset($_COOKIE['usernameCookie'])) {
+            unset($_COOKIE['usernameCookie']);
+            setcookie('usernameCookie', '', time() - 3600, '/'); // empty value and old timestamp
+        }
         setcookie('usernameCookie', $user,0,'/');
-        // header("Location: ../Front-End/Homepage.html");
-        header("Location: ../Back-End/ProfilePage.php");
+        header("Location: ../Front-End/Homepage.html");
     }
     else{
         header("Location: ../Front-End/login.php?error=1");
