e5d009d26895f592ac5e80e7e31aa15a27f7b0e3
devinsalemmo <devinsal@buffalo.edu>
2023-04-13 15:22:45 -0400

    Created Front end messaging page #51

--- Front-End/Homepage.php
+++ Front-End/Homepage.php
@@ -3,10 +3,10 @@
 require_once('../Back-End/DBConn.php');
 
 if(!isset($_SERVER['HTTPS'])||$_SERVER['HTTPS']!='on'){
-    header('Location: '.
-    'https://'.
-    $_SERVER['SERVER_NAME'].
-    $_SERVER['PHP_SELF']);
+    //header('Location: '.
+    //'https://'.
+    //$_SERVER['SERVER_NAME'].
+   //// $_SERVER['PHP_SELF']);
 }
 
 $dbConn = new DBConn();
